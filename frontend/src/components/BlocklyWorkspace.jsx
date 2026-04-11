5c80a55660b0c14a82da18af32e3ec331b2498ca
R9jin <ticsrgt@gmail.com>
2026-04-11 19:51:51 +0800

    transferring backend to render

--- frontend/src/components/BlocklyWorkspace.jsx
+++ frontend/src/components/BlocklyWorkspace.jsx
@@ -426,7 +426,11 @@ const BlocklyWorkspace = forwardRef(({ onChange, syntaxError }, ref) => {
   const workspace = useRef(null);
   const onChangeRef = useRef(onChange);
 
+<<<<<<< HEAD
   const API_URL = import.meta.env.VITE_BACKEND_URL || "";
+=======
+  const API_URL = import.meta.env.VITE_BACKEND_URL || ""
+>>>>>>> 5328ff5 (transferring backend to render)
 
   useImperativeHandle(ref, () => ({
     clear: () => {
